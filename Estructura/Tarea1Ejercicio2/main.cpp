30bae20b4c1a3ee690e644cd9e1af92b330d4876
colina118 <mikehill2010@gmail.com>
2015-01-29 21:07:31 -0600

    3.commit

--- Estructura/Tarea1Ejercicio2/main.cpp
+++ Estructura/Tarea1Ejercicio2/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+#include <cstdlib>
+#include <Time.h>
 
 template <typename T>
 void imprimeArreglo(T **, int, int);
-
+bool segunda(int **, int, int, int, int);
 int main(int argc, char **argv)
 {
+	srand(time(NULL));
+	
 	int reng;
 	int colu;
 	int ** matriz;
-    //srand( (unsigned int) time(nullptr));
+    
 	
 	std::cout << "Mete el numero de renglones" << std::endl;
 	std::cin >> reng;
@@ -20,6 +24,21 @@ int main(int argc, char **argv)
     {
         matriz[i] = new int[colu];
     }
+	
+	for (int i = 0; i < reng; ++i)
+		for(int j = 0; j < colu; j++)
+		{
+			{
+				matriz[i][j] = rand() % 2;
+			}
+		}
+	imprimeArreglo<int>(matriz, reng, colu);
+	bool siSaliste = segunda (matriz, 0, 0, reng, colu);
+	
+	if(siSaliste)
+		std::cout << "llegaste a la salida";
+	else
+		std::cout << "No hay solucion"; 
 	
 	return 0;
 }
@@ -36,3 +55,31 @@ void imprimeArreglo(T **v, int ren, int col)
 			std::cout << std::endl;
 		}
 }
+
+bool segunda(int **matriz, int ren, int col, int renA, int colA)
+{
+	int valor = matriz[ren][col];
+	if(valor == 1 && ren == renA - 1 && col == colA - 1)		
+		return true;
+	else if(matriz[ren + 1][col] == 1)
+		{
+			std::cout << "Avanze una casilla hacia abajo" << std::endl;
+			if(ren == renA)
+				return segunda(matriz, ren, col + 1, renA, colA);
+			else
+				return segunda(matriz, ren + 1, col, renA, colA);
+		}
+	else if(matriz[ren][col + 1] == 1)
+		{
+			std::cout << "Avanze una casilla a la derecha " << std::endl;
+			if ( col == colA)
+				return segunda(matriz, ren, col, renA, colA);
+			else
+				return segunda(matriz, ren, col + 1, renA, colA);
+		}
+		else 
+		{
+			return false;
+		}
+}
+
